7a86b04d841b0e230b7398f353d16a980f367d91
tycrek <josh.moore@jmoore.dev>
2022-02-09 11:48:13 -0700

    Renamed class

---
+++ src/main/java/io/ttrms/skeleton/Handler.java
@@ -0,0 +1,24 @@
+package io.ttrms.skeleton;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public abstract class Handler<T, CoreT, SessionT, PacketT> {
+    @Getter(AccessLevel.PROTECTED) private final CoreT core;
+
+    /**
+     * Do something with the provided packet and decides if it should be forwarded to a connected player
+     *
+     * @return true if the packet should be forwarded, false to drop the packet
+     */
+    @SuppressWarnings("unchecked")
+    public boolean handle(SessionT session, PacketT packet) {
+        return apply(session, (T) packet);
+    }
+
+    protected abstract boolean apply(SessionT session, T packet);
+
+    protected abstract IStore getStore();
+}
