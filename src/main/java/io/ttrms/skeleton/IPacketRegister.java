22ad0fdf89a9a9d0f2adb9380f12a1c5bb9873fb
tycrek <josh.moore@jmoore.dev>
2022-02-08 18:09:59 -0700

    woops 0.1-9

--- src/main/java/io/ttrms/skeleton/IPacketRegister.java
+++ src/main/java/io/ttrms/skeleton/IPacketRegister.java
@@ -1,6 +1,6 @@
 package io.ttrms.skeleton;
 
 public interface IPacketRegister<Register extends IPacketRegister<?, ?, Packet>, Handler extends Handlers.Handler<?, ?, ?, Packet>, Packet> {
-    Register add(Class<? extends Packet> packetClass, Handlers.Handler<?, ?, ?, ?> handler);
+    Register add(Class<? extends Packet> packetClass, Handler handler);
     Handler getHandler(Packet packet);
 }
