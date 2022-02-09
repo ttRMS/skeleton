65fd7bb5aa631c2f55ec93715ed779a8ecb65323
tycrek <josh.moore@jmoore.dev>
2022-02-08 17:48:52 -0700

    REEEEEEEEEEEE (0.1-7)

--- src/main/java/io/ttrms/skeleton/AbstractPacketManager.java
+++ src/main/java/io/ttrms/skeleton/AbstractPacketManager.java
@@ -2,7 +2,9 @@ package io.ttrms.skeleton;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @AllArgsConstructor
 public abstract class AbstractPacketManager<Core extends AbstractCore<?, ?, ?, ?, ?, ?, ?, ?, ?>, ServerListener, ClientListener> {
     @Getter protected final Core core;
